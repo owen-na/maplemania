1d10d3d4512ac8adbc28e1a38677fd054c653e59
owen <osn53387@uga.edu>
2024-06-22 16:32:22 -0400

    began implentation of api caller to mapleranks

--- src/app/Planner/Planner.tsx
+++ src/app/Planner/Planner.tsx
@@ -1,4 +1,5 @@
 import TaskHolder from "@/Components/TaskHolder/TaskHolder";
+import { useState } from "react";
 import styles from "./Planner.module.css"
 
 export default function Planner() {
@@ -7,10 +8,47 @@ export default function Planner() {
             <TaskHolder title = "Gear Upgrades"/>
             <TaskHolder title = "Farming Goals"/> 
             <div className={styles.charDisplay}>
-                <p>Parsing MapleRanks or Maplestory rankings page for banner of main character</p>
+                {/* <p>Parsing MapleRanks or Maplestory rankings page for banner of main character</p> */}
+                {nameGetter()}
             </div>
             {/* gear upgrades, farming goals, estimated costs*/}
              {/* need it where it has to be showing off reccomendations? not sure yet*/}
         </div>
     )
 }
+
+function nameGetter() {
+
+    const [region, setRegion] = useState('1'); // Default to 'NA'
+    const [inGameName, setInGameName] = useState('');
+
+    return (
+        <>
+         <form>
+            <select>
+                <option value={1}>NA</option>
+                <option value={2}>EU</option>
+            </select>
+            <input type="text" placeholder="in-game name"></input>
+            <input type="submit" value="Search"></input>
+         </form>
+        </>
+    )
+}
+
+//  × You're importing a component that needs useState. It only works in a Client Component but none of its parents are marked with "use client", so they're Server Components by default.
+//   │ Learn more: https://nextjs.org/docs/getting-started/react-essentials
+//   │ 
+//   │ 
+//    ╭─[/Users/owen/StudioProjects/maplemania/src/app/Planner/Planner.tsx:1:1]
+//  1 │ import TaskHolder from "@/Components/TaskHolder/TaskHolder";
+//  2 │ import { useState } from "react";
+//    ·          ────────
+//  3 │ import styles from "./Planner.module.css"
+//  4 │ 
+//  5 │ export default function Planner() {
+//    ╰────
+
+// Import trace for requested module:
+// ./src/app/Planner/Planner.tsx
+// ./src/app/page.tsx
