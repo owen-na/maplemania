80ad60da3a1db5da5b7ec1274e645f44429b8c71
owen-na <owensungminna@gmail.com>
2024-06-23 01:44:19 -0400

    Created TODO and extra resource consderation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,16 @@ import NavBar from "../Components/Navbar/Navbar";
 import Dailies from "./Dailies/Dailies";
 import Planner from "./Planner/Planner";
 
+// https://www.chartjs.org/docs/latest/charts/radar.html
+// for radar graph i really liked the look of to use with the web scrape later
+
+// TODO
+// Web Scraping off maple ranks (Currently have another branceh to try it out with puppetter)
+// Add proper anchors for the navbar
+// create a footer
+// begin on actaully dailies content
+
+
 export default function Home() {
   return (<>
     <NavBar />
