ef4963b3599e237fa329a6fe31362dea33daa253
owen-na <owensungminna@gmail.com>
2024-06-27 13:40:53 -0400

    began on caching for storing checkbox on refresh

--- src/Components/InfoNodeLong/LongInfoNode.tsx
+++ src/Components/InfoNodeLong/LongInfoNode.tsx
@@ -1,10 +1,32 @@
+'use client';
+
+import { ChangeEvent, useEffect, useState } from "react";
 import styles from "./LongInfoNode.module.css"
 import Image from "next/image"
 
-export default function LongInfoNode({imageHref, desc} : {imageHref : string, desc : String}) {
+
+export default function LongInfoNode({imageHref, desc} : {imageHref : string, desc : string}) {
+
+    // const [checkedItems, setCheckedItems] = useState<{ [key: string]: boolean }>({});
+
+    // const handleChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    // const response = await fetch("./api/route")
+    // const {checkedItems} = response.json()
+    //   setCheckedItems({
+    //     ...checkedItems,
+    //     [event.target.name]: event.target.checked,
+    //   });
+    // };
+
     return (
             <li className={styles.box}> 
-                <input type="checkbox" className={styles.checker}></input>
+                <input 
+                type="checkbox" 
+                className={styles.checker}
+                name={desc} // Use `desc` as the key for the checkbox state
+                // checked={checkedItems[desc] || false} // Set checked based on state
+                // onChange={handleChange}
+                 />
                 <p>{desc}</p>
                 <Image
                 src={imageHref}
